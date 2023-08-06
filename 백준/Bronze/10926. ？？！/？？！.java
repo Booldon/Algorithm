cdf1174d0ea8d292fe132d981316d055361c99b9
Booldon <99729203+Booldon@users.noreply.github.com>
2023-08-06 22:55:01 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = a;
+
+        if(a==b){
+            System.out.println(a+"??!");
+        }
+    }
+}
